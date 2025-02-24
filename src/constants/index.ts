b71337640f8cd3c32cd6ebd8f7d75b51ea16ff99
Udo Bauer <knackwurstking.tux@gmail.com>
2025-02-24 17:42:37 +0100

    Update version to v0.1.1 (build: 2)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export const version = "v0.1.0";
-export const build = 1;
+export const version = "v0.1.1";
+export const build = 2;
 export const slotTitles = ["A / B", "C / D", "E / F", "G / H", "I / J"];

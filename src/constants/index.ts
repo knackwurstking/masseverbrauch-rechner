5e2012ef257cb77de632ce8c2e42cc1ff2491de3
Udo Bauer <knackwurstking.tux@gmail.com>
2025-02-24 17:48:47 +0100

    fix: version and build number, now v0.1.1 [build: 3]

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export const version = "v0.1.1";
-export const build = 2;
+export const version = "v0.2.0";
+export const build = 3;
 export const slotTitles = ["A / B", "C / D", "E / F", "G / H", "I / J"];

8c601d8e9779fab5ca03131faf96cce2d82b6dad
Udo Bauer <knackwurstking.tux@gmail.com>
2025-03-05 04:16:25 +0100

    fix: vite assets icon paths

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export const version = "v0.2.1";
-export const build = 4;
+export const version = "v0.2.2";
+export const build = 5;
 export const slotTitles = ["A / B", "C / D", "E / F", "G / H", "I / J"];
